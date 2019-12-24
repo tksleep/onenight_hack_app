d3141eb61c811c324e04df8f2758ddda51227391
tektomo <taobmcoe@outlook.com>
2019-12-25 04:15:04 +0900

    Add database

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import requests
 import json
+import os
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db_uri = "sqlite:///" + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
 GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+
+
+class Data(db.model):
+    name = db.Column(db.String(), nullable=False)
+    title = db.Column(db.String(), nullable=False)
+    url = db.Column(db.String(), nullable=False)
 
 
 @app.route('/', methods=['GET'])
@@ -22,6 +33,12 @@ def check_present():
     if data["totalItems"] != 1:
         return render_template("present.html", result=False)
     else:
+        present = Data()
+        present.name = request.form["name"]
+        present.title = data["items"][0]["volumeInfo"]["title"]
+        present.url = "https://www.amazon.co.jp/dp/" + isbn
+        db.session.add(present)
+        db.session.commit()
         return render_template("present.html", name=request.form["name"],
                                result=True,
                                title=data["items"][0]["volumeInfo"]["title"],
@@ -29,10 +46,11 @@ def check_present():
                                + isbn)
 
 
-"""@app.route('/getpresent', methods=["POST"])
+@app.route('/getpresent', methods=["POST"])
 def get_present():
+    got_present = db.session.query(Data).order_by(db.func.random()).first()
+    print(got_present)
 
-"""
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
